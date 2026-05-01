6faba80850bd1ede6be45b14caea40ef10240b95
enescelik550-lgtm <enescelik550@gmail.com>
2026-05-01 22:48:16 +0200

    Junior updates savings function

--- finance.py
+++ finance.py
@@ -1,6 +1,5 @@
 def calculate_savings(income, expenses):
-    return income - expenses
-
+    return float(income - expenses)
 
 def discount_price(price, discount_percent):
     return price - (price * discount_percent / 100)
