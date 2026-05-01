800f5eded56dc70cbf2e382213a94af8b212b989
enescelik550-lgtm <enescelik550@gmail.com>
2026-05-01 22:24:20 +0200

    Add tax calculator feature

--- finance.py
+++ finance.py
@@ -4,3 +4,6 @@ def calculate_savings(income, expenses):
 
 def discount_price(price, discount_percent):
     return price - (price * discount_percent / 100)
+
+def calculate_tax(amount, tax_percent):
+    return amount * tax_percent / 100
